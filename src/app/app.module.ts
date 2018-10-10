c0d2fc788aaf641efa27e55e0a9babac8b59242d
yabastidas <yabastidas@YAB>
2018-10-10 15:14:41 -0500

    version1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const config = {
+  apiKey: "AIzaSyBZIcSRLtnzxd8nhFAYZO8VEP4I06EQjjg",
+    authDomain: "dbpost-c2fb0.firebaseapp.com",
+    databaseURL: "https://dbpost-c2fb0.firebaseio.com",
+    projectId: "dbpost-c2fb0",
+    storageBucket: "dbpost-c2fb0.appspot.com",
+    messagingSenderId: "121551071603"
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config, "dbpost-c2fb0"),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +41,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tasksRef: AngularFireList<any>;
+  items: Observable<any[]>;
 
+  constructor(public navCtrl: NavController, db: AngularFireDatabase) {
+  debugger
+    //this.items = db.list('dbprueba').valueChanges();
+    this.tasksRef = db.list('dbposts');
+    this.items = this.tasksRef.snapshotChanges().pipe(
+          map((e:Response)=> {
+            debugger
+            return e.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+          })
+        );
+    console.log(this.items);
   }
 
 }
